e2dd383fee841bb26b931f1662bdf6a37b9fcae9
MostafaCse <mdmostafaiiuc@gmail.com>
2020-08-07 21:09:31 +0600

    advanced js

---
+++ trueOrfalse.js
@@ -0,0 +1,16 @@
+// false 0,"",null,undefined,Nan
+//true -1< and 1> except 0."0"," "
+let age=0;
+if(age){
+    console.log("condititon is true");
+}
+else{
+    console.log("condition is false");
+}
+let name="";
+if(name){
+    console.log("condititon is true");
+}
+else{
+    console.log("condition is false");
+}
